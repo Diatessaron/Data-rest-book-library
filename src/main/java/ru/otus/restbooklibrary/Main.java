3915dd6d682195b43a52359743696bf200e1731b
Azat Fazlyev <afazlyev436@gmail.com>
2021-04-03 23:11:41 +0300

    Initial commit

---
+++ src/main/java/ru/otus/restbooklibrary/Main.java
@@ -0,0 +1,11 @@
+package ru.otus.restbooklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
